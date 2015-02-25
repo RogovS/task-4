047b47eff2da86b467ad22956e17856bb214df3b
Сергей <sergy7@mail.ru>
2015-02-25 20:25:13 +0000

    First Commit

--- myApp.js
+++ myApp.js
@@ -14,7 +14,7 @@ app.get('/', function (request, response) {
 
 app.use(express.static(__dirname + '/public'));
 
-var io = require('socket.io').listen(app.listen(port));
+var io = require('socket.io').listen(server);
 io.on('connection', function () {
 	io.set('transports',['xhr-polling']);
 	io.set("polling duration", 10);
