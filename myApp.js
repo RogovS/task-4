d67b01bd64d48aafa657e556863d17a93b1f6cca
Сергей <sergy7@mail.ru>
2015-02-25 19:23:02 +0000

    First Commit

--- myApp.js
+++ myApp.js
@@ -20,7 +20,7 @@ var io = require('socket.io').listen(server.io.configure(function () {
 		}));
 
 		io.sockets.on('connection', function (socket) {
-		   socket.emit('news');
+		   socket.emit('news', { hello: 'world' });
 		   socket.on('my other event', function (data) {
 			  console.log(data);
 		   });
