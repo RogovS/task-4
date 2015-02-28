22d71f51acb4eb60f2156afbe5cfc78266b84a1e
Сергей <sergy7@mail.ru>
2015-02-28 08:00:48 +0000

    Second Commit

--- myApp.js
+++ myApp.js
@@ -30,7 +30,7 @@ io.on('connection', function (socket) {
 var mongo = require('mongoskin');
 var conn = mongo.db('mongodb://RogovS:5mongo$*@ds049641.mongolab.com:49641/task-5');
 
-conn.collection('collectionName').update(
+/*conn.collection('collectionName').update(
 {
    user:"userToUpdate"
 },
@@ -45,7 +45,7 @@ conn.collection('collectionName').update(
 conn.collection('collectionName').findOne( {
    user:"userToFind"
 },
-function(err, doc) {
-   if (err) { /* something is wrong */ }
-   if (doc) { var foundData = doc.someData; }
-});
+function(err, doc) {*/
+   //if (err) { /* something is wrong */ }
+   //if (doc) { var foundData = doc.someData; }
+//});
