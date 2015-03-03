b5ac4316c7e581971bbe7843c5dc6a4fedff292a
Сергей <sergy7@mail.ru>
2015-03-03 11:57:34 +0000

    First Commit

--- myApp.js
+++ myApp.js
@@ -27,10 +27,10 @@ io.on('connection', function (socket) {
 	 });
 });
 
-/*var mongo = require('mongoskin');
+var mongo = require('mongoskin');
 var conn = mongo.db('mongodb://RogovS:5mongo$*@ds049641.mongolab.com:49641/task-5');
 
-onn.collection('collectionName').update(
+conn.collection('collectionName').update(
 {
    user:"userToUpdate"
 },
@@ -45,7 +45,7 @@ onn.collection('collectionName').update(
 conn.collection('collectionName').findOne( {
    user:"userToFind"
 },
-function(err, doc) {*/
-   //if (err) { /* something is wrong */ }
-   //if (doc) { var foundData = doc.someData; }
-//});
+function(err, doc) {
+   if (err) { /* something is wrong */ }
+   if (doc) { var foundData = doc.someData; }
+});
