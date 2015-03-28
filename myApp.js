f24b94512683922332f8645fb02ca837125ffc62
Сергей <sergy7@mail.ru>
2015-03-28 05:32:46 +0000

    First Commit

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 3000;*/
 
 /////////////////////////////////////////////
 
-var WebSocketServer = require("ws").Server;
+/*var WebSocketServer = require("ws").Server;
 var http = require("http");
 var express = require("express");
 var app = express();
@@ -38,7 +38,41 @@ wss.on("connection", function(ws) {
     clearInterval(id);
   });
 });
+*/
 
+var express = require('express')
+var http = require('http');
+var app = express();
+var server = http.createServer(app);
+var port = process.env.PORT || 3000;
+ 
+server.listen(port, function() {
+   console.log('Listening on ' + port);
+});
+ 
+app.get('/', function (request, response) {
+   response.sendfile(__dirname + '/index.html');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+var WebSocketServer = require("ws").Server
+var wss = new WebSocketServer({server: server})
+ 
+wss.on("connection", function(ws) {
+  console.log("websocket connection open")
+  var msg = {
+      type: "news",
+      data: "hello world"
+  }
+  ws.send(JSON.stringify(msg));
+ 
+  ws.onmessage = function(d) {
+    msg = JSON.parse(d.data);
+    console.log("websocket messsage received")
+    console.log(msg);
+  };
+});
 
 /////////////////////////////////////////////
 
