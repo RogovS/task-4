480e1833f84563da1133d26f5eefde63cf5f6b67
Сергей <sergy7@mail.ru>
2015-04-11 06:05:33 +0000

    First Commit

--- myApp.js
+++ myApp.js
@@ -1,8 +1,11 @@
-var express = require('express')
+var express = require('express');
 var http = require('http');
 var app = express();
 var server = http.createServer(app);
 var port = process.env.PORT || 3000;
+
+var mongo = require('mongoskin');
+var db = mongo.db('mongodb://RogovS:5mongo@ds049641.mongolab.com:49641/task-5');
  
 server.listen(port, function() {
    console.log('Listening on ' + port);
@@ -14,37 +17,24 @@ app.get('/', function (request, response) {
 
 app.use(express.static(__dirname + '/public'));
 
-var WebSocketServer = require("ws").Server
-var wss = new WebSocketServer({server: server})
+var WebSocketServer = require("ws").Server;
+var wss = new WebSocketServer({server: server});
  
 wss.on("connection", function(ws) {
-  console.log("websocket connection open")
+  console.log("websocket connection open");
   var msg = {
       type: "news",
       data: "hello world"
-  }
+  };
   ws.send(JSON.stringify(msg));
  
   ws.onmessage = function(d) {
     msg = JSON.parse(d.data);
-    console.log("websocket messsage received")
+    console.log("websocket messsage received");
     console.log(msg);
   };
-});
-
-/////////////////////////////////////////////
-
-    var mongo = require('mongoskin');
-    var db = mongo.db('mongodb://RogovS:5mongo@ds049641.mongolab.com:49641/task-5');
+  
     console.log("проверка");
-    
-    //conn.collection('TestCollection').insert( { ip: 'ip', date: "someDate" } );
-    
-    /*conn.createCollection("test", function(err, collection){
-      collection.insert({"test":"value"});
-    });*/
-    
-    //conn.collection.insert();
     
     db.collection('TestCollection').update(
         {
@@ -61,6 +51,38 @@ wss.on("connection", function(ws) {
             upsert:true
         }
     );
+  
+});
+
+/////////////////////////////////////////////
+
+    /*var mongo = require('mongoskin');
+    var db = mongo.db('mongodb://RogovS:5mongo@ds049641.mongolab.com:49641/task-5');
+    console.log("проверка");*/
+    
+    //conn.collection('TestCollection').insert( { ip: 'ip', date: "someDate" } );
+    
+    /*conn.createCollection("test", function(err, collection){
+      collection.insert({"test":"value"});
+    });*/
+    
+    //conn.collection.insert();
+    
+    /*db.collection('TestCollection').update(
+        {
+            id:"123"
+            
+        },
+        {
+            id:"123",
+            ip:"123.123.123.123",
+            date:"11.04.2015"
+            
+        },
+        {
+            upsert:true
+        }
+    );*/
     
     /*db.collection('TestCollection').update(
     {
